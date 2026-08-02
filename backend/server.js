dd6019776d7319174d66ceee05a2756bf2ff0ff7
Mohammad Iqbal <mochammad_iqbal_x@telkomsel.co.id>
2026-08-02 14:35:00 +0700

    fix: import fs module in server.js to resolve ReferenceError

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { JsonRepository } from './src/repositories/baseRepository.js';
